c3a0abe4fef595882c68511bbfd4bffacc06431f
deepesh.rehi <tl542nn6ieco2mxms3ush5limwivgzkhx4ginjxgwbk6mtvkrxyq>
2025-10-24 01:47:37 +0800

    1st Push

---
+++ src/main/java/com/scaler/productservicecapstoneapplication/ProductServiceCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicecapstoneapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceCapstoneApplication.class, args);
+    }
+
+}
